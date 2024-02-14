53feb530f60fceb7aa1e871d90b64c4e821381da
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-02-14 23:29:55 +0530

    Adding signup routes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -8,4 +10,7 @@ app.get("/",(req,res)=>{
     res.send("Hello world")
 })
 
-app.listen(PORT,()=>console.log('Server is running on 5000'))
+app.get("/api/auth/signup",(req,res)=>{
+    console.log("Signup route");
+});
+app.listen(PORT,()=>console.log(`Server is running on ${PORT}`))
