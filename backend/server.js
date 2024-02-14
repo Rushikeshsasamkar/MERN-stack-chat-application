2399e0d14a4af8bb70e1fcee8e9e66139070df2d
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-02-14 23:16:41 +0530

    Adding .env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000,()=>console.log('Server is running on 5000'))
+app.get("/",(req,res)=>{
+    // route route 
+    res.send("Hello world")
+})
+
+app.listen(PORT,()=>console.log('Server is running on 5000'))
