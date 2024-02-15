1b0c54b18f74343665f426a8aea92a4f1cbbff3b
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-02-15 21:42:10 +0530

    Hashed the password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,8 @@
 import User from "../models/userModel.js";
+import bcrypt from 'bcryptjs'
+
+
+
 
 export const signup = async(req,res)=>{
     try {
@@ -10,8 +14,12 @@ export const signup = async(req,res)=>{
 
         const user = await User.findOne({username})
         if(user){
-            return res.status(400).json({error:"Password did'nt match"})
+            return res.status(400).json({error:"User already exists"})
         }
+
+          // HASH Password Here
+          const salt = await bcrypt.genSalt(10);
+          const hashedPassword = await bcrypt.hash(password,salt);
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
 		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
@@ -19,21 +27,27 @@ export const signup = async(req,res)=>{
         const newUser = new User({
             fullname,
             username,
-            password,
+            password: hashedPassword,
             gender,
             profilePic: gender === 'male' ? boyProfilePic: girlProfilePic
         })
 
-        await newUser.save();
+       
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullname: newUser.fullname,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+        if(newUser){
+            // Generate JWT Token
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        }else{
+            res.status(500).json({error:"Invaid user data!"});
+        }
 
-        // HASH Passoword Here
+      
     } catch (error) {
         console.log("Error in signup controller",error.message);
         res.status(500).json({error:"Internal server error"})
