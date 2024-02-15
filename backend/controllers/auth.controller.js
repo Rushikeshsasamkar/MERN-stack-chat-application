dc70dfc1744bd56a432fe48abc4c5dc3e6acf20a
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-02-15 18:18:27 +0530

    confirm password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,18 @@
+import User from "../models/userModel.js";
+
 export const login = async(req,res)=>{
     try {
         const {fullname, username, password,confirmPassword} = req.body;
+    
+        if(password!== confirmPassword){
+            return res.status(400).json({error:"Password did'nt match"})
+        } 
+
+        const user = await User.findOne(username)
+        if(user){
+            return res.status(400).json({error:"Password did'nt match"})
+        }
+
     } catch (error) {
         
     }
